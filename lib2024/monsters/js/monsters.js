f5e1ebd65a435eb61edcc734a47f2a5e17ac9ded
Jakob <122372866+camp1054@users.noreply.github.com>
2023-03-10 19:38:54 -0500

    firstbigpush

---
+++ lib2024/monsters/js/monsters.js
@@ -0,0 +1,6 @@
+function change() // no ';' here
+{
+    var elem = document.getElementById("hplbutton");
+    if (elem.value=="Iä! Iä!") elem.value = "P̵h̷'̷n̴g̸l̵u̷i̸ ̴m̵g̸l̷w̶'̷n̸a̶f̵h̷ ̵C̶t̵h̷u̸l̶h̴u̶ ̸R̵'̸l̷y̶e̵h̴ ̸w̷g̷a̶h̸'̵n̶a̴g̷l̵";
+    else elem.value = "F̴̢̗̜͔̪̻̳́͒͌͘ͅḩ̵͙̜̹̳̭̜͕̤̤̌̉̇͂̂̾̌̋͌̿̚͘̕͝ͅt̵̢̜͑̄̏͗̂̑̐͌̓̍ą̷̡̜̟͔̻̣͎͖̥͊̇̅͗g̸̢̮̳̝͎͕̬̙̮̗̔̀̿͑̈̃̕ņ̴̼͔̙̥̜̮̫̳̙͍̖̙̙͈̈́̇̀̄̌̃͌̍͂̈́̀̒̑͝!̴̜͌͊͛̃͊̕͘͝͝";
+}
